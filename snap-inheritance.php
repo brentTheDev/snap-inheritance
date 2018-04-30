2e3528d315e57c95e68097270a285efb1c83ac30
Brent Kie <brent.a.kie@gmail.com>
2018-04-30 08:50:53 -0600

    Created constructor, accessor and mutator methods for Vehicle class

--- snap-inheritance.php
+++ snap-inheritance.php
@@ -2,7 +2,35 @@
 
 
 class Vehicle {
-	protected $licensePlate;
+	private $licensePlate;
+
+	public function __construct(string $licensePlate) {
+		try {
+			$this->setLicensePlate($newLicensePlate);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+	}
+	public function getLicensePlate(): string{
+		return ($this->licensePlate);
+		}
+
+	public function setLicensePlate($newLicensePlate): void{
+			$newLicensePlate = trim($newLicensePlate);
+			$newLicensePlate = filter_var($newLicensePlate, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		}
+
+		if(empty($newLicensePlate) === true) {
+		throw(new \InvalidArgumentException("genre is insecure"));
+		}
+		//verifies if the fan username is less than 8 characters
+		if(strlen($newLicensePlate) >= 8)
+			throw(new \RangeException("genre description cannot fit in mySQL"));
+		// store the new fan username
+		$this->licensePlate = $newLicensePlate;
+
+}
 
 	public function tune(): void {
 		echo "Let Senator Arlo paw at it!" . PHP_EOL;
